cf1523118d6e726a2856f8c7c918b68d60001e77
elisimo98 <69318466+elisimo98@users.noreply.github.com>
2020-09-12 20:06:04 +0100

    Update Questionnaire.php

--- app/Models/Questionnaire.php
+++ app/Models/Questionnaire.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Questionnaire extends Model
 {
     protected $fillable = ['name'];
+
+    public function question(){
+        return $this->hasMany(Question::class);
+    }
+}
+
+$questionniares = Questionnaire::with('Question')->get();
+
+foreach ($questionniares as $questionnaire) {
+    echo $questionnaire->Question;
 }
