5e63f9406cd5493dbd38c8324f39e51354d5e910
elisimo98 <69318466+elisimo98@users.noreply.github.com>
2020-09-12 20:05:30 +0100

    Update Question.php

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    public function questionnaire(){
+        return $this->belongsTo(Questionnaire::class);
+    }
+
 }
